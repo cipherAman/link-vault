bdf1a57f0c8f22c1f5fa2ac2004924afaef2351b
Aman <aman.enthusiast@gmail.com>
2026-06-14 23:58:47 +0530

    missing / in api path

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGO_URI)
     .catch((err)=>console.log(err));
 
 
-app.use('api/auth',authRoutes);
-app.use('api/links',linkRoutes);
+app.use('/api/auth',authRoutes);
+app.use('/api/links',linkRoutes);
 
 module.exports =app;
