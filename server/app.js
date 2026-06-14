be9f0afe0dfd17653e6f897c1e3c6da72248a24f
Aman <aman.enthusiast@gmail.com>
2026-06-14 17:05:37 +0530

    express server done with mongoose uri

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+const authRoutes = require('./routes/auth.routes');
+const linkRoutes = require('./routes/link.routes');
+
+const app=expres();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>console.log('MongoDB connected'))
+    .catch((err)=>console.log(err));
+
+
+app.use('api/auth',authRoutes);
+app.use('api/links',linkRoutes);
+
+module.exports =app;
