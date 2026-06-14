16b0b2bdab0c897432d0eae806867c7a03b08b60
Aman <aman.enthusiast@gmail.com>
2026-06-14 22:50:42 +0530

    jwt writing typo

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 const User=require("../models/User.model");
-const jwt =require('jwt');
+const jwt =require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
 exports.register = async (req,res)=>{
