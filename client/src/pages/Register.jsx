5c68a8fb215e21129e2becbb4b06676b92358e0e
Aman <aman.enthusiast@gmail.com>
2026-06-15 14:54:37 +0530

    fixed axios path in register

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useAuth } from '../context/' ;
 import { useNavigate, Link } from 'react-router-dom';
-import axios from 'axios';
+import axios from '../api/axios';
 
 
 const Register = ()=>{
